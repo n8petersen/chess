13ea69bcc2765a3458dc55a50c6df2973cec5fda
n8petersen <n8knows@gmail.com>
2024-05-02 22:44:25 -0600

    change switch

--- shared/src/main/java/chess/moves/MoveCalculator.java
+++ shared/src/main/java/chess/moves/MoveCalculator.java
@@ -13,23 +13,14 @@ public class MoveCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece currentPiece = board.getPiece(myPosition);
         ChessPiece.PieceType currentType = currentPiece.getPieceType();
-        ArrayList<ChessMove> possibleMoves = new ArrayList<ChessMove>();
 
-        switch (currentType) {
-            case KING:      possibleMoves = new KingMoves().getMoves();
-                            break;
-            case QUEEN:     possibleMoves = new QueenMoves().getMoves();
-                            break;
-            case BISHOP:    possibleMoves = new BishopMoves().getMoves();
-                            break;
-            case KNIGHT:    possibleMoves = new KnightMoves().getMoves();
-                            break;
-            case ROOK:      possibleMoves = new RookMoves().getMoves();
-                            break;
-            case PAWN:      possibleMoves = new PawnMoves().getMoves();
-                            break;
-        }
-
-        return possibleMoves;
+        return switch (currentType) {
+            case KING -> new KingMoves().getMoves();
+            case QUEEN -> new QueenMoves().getMoves();
+            case BISHOP -> new BishopMoves().getMoves();
+            case KNIGHT -> new KnightMoves().getMoves();
+            case ROOK -> new RookMoves().getMoves();
+            case PAWN -> new PawnMoves().getMoves();
+        };
     }
 }

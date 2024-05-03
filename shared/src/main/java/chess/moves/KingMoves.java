0e3245ae38c26926e92b718779f39c091db7febb
n8petersen <n8knows@gmail.com>
2024-05-03 13:38:52 -0600

    add parameters

--- shared/src/main/java/chess/moves/KingMoves.java
+++ shared/src/main/java/chess/moves/KingMoves.java
@@ -1,12 +1,14 @@
 package chess.moves;
 
+import chess.ChessBoard;
 import chess.ChessMove;
+import chess.ChessPosition;
 
 import java.util.ArrayList;
 
 public class KingMoves {
 
-    public ArrayList<ChessMove> getMoves() {
+    public ArrayList<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessMove> moveList = new ArrayList<ChessMove>();
 
         return moveList;

c5dcd26a33c2544aa8043f634cada4915b9f3f4b
n8petersen <n8knows@gmail.com>
2024-05-03 23:24:21 -0600

    add TODO comments

--- shared/src/main/java/chess/moves/RookMoves.java
+++ shared/src/main/java/chess/moves/RookMoves.java
@@ -1,13 +1,21 @@
 package chess.moves;
 
+import chess.ChessBoard;
 import chess.ChessMove;
+import chess.ChessPosition;
 
 import java.util.ArrayList;
 
 public class RookMoves {
 
-    public ArrayList<ChessMove> getMoves() {
+    public ArrayList<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessMove> moveList = new ArrayList<ChessMove>();
+
+        // TODO:    Will look similar to previous moves we've done, checking straights
+        //          Use a while loop, incrementing the move in the same direction until there is a piece blocking, or edge of board
+        //          If piece is blocking, check color:
+        //          If own color, don't add the move and stop loop
+        //          If other color, add move and stop loop
 
         return moveList;
     }

df4cf4a8b53db76c5fa9c02cae96d664a8b4f958
n8petersen <n8knows@gmail.com>
2024-05-04 17:59:28 -0600

    remove knight todo notes

--- shared/src/main/java/chess/moves/KnightMoves.java
+++ shared/src/main/java/chess/moves/KnightMoves.java
@@ -9,11 +9,6 @@ public class KnightMoves {
     public ArrayList<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessMove> moveList = new ArrayList<ChessMove>();
 
-        // TODO:    This one will be a little unique, but similar to the King, as there are a set of specific moves
-        //          Doesn't need to check for blocking, or loop
-        //          Just check for edge of board, and for own pieces for invalid moves to NOT add
-        //          Any moves that are in the board with null pieces, or opposite color pieces add as valid moves
-
         moveList.addAll(addMoves(board, myPosition, 2,-1));
         moveList.addAll(addMoves(board, myPosition, 2,1));
 

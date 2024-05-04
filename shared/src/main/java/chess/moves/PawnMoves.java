3a143ff6c10cecc495e64e282594f1039245f462
n8petersen <n8knows@gmail.com>
2024-05-04 17:59:40 -0600

    add pawn moves. todo: add promotions

--- shared/src/main/java/chess/moves/PawnMoves.java
+++ shared/src/main/java/chess/moves/PawnMoves.java
@@ -4,10 +4,28 @@ import chess.*;
 
 import java.util.ArrayList;
 
+import static java.lang.Math.abs;
+
 public class PawnMoves {
 
     public ArrayList<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessMove> moveList = new ArrayList<ChessMove>();
+
+        // TODO: add promotions
+        ChessGame.TeamColor color = board.getPiece(myPosition).getTeamColor();
+        int dir = (color == ChessGame.TeamColor.WHITE ? 1 : -1);
+
+        // add fwd moves
+        moveList.addAll(addMoves(board, myPosition, dir, 0, false));
+        // add attacking moves
+        moveList.addAll(addMoves(board, myPosition, dir, 1, true));
+        moveList.addAll(addMoves(board, myPosition, dir, -1, true));
+        // add starter row moves
+        ChessPosition checkPosition = new ChessPosition(myPosition.getRow() + dir, myPosition.getColumn());
+        ChessPiece checkPiece = board.getPiece(checkPosition);
+        if ((myPosition.getRow() == 2 || myPosition.getRow() == 7) && checkPiece == null) {
+            moveList.addAll(addMoves(board, myPosition, 2 * dir, 0, false));
+        }
 
         return moveList;
     }
@@ -20,15 +38,18 @@ public class PawnMoves {
 
         // check if out of edge of board
         if (newRow < 9 && newRow > 0 && newCol < 9 && newCol > 0) {
-            // TODO after checking if edge of board, check:
-            // TODO - If attack mode, check that there is an enemy on spot
-            // TODO -   else, check that there is NO piece, regardless of color
-
-            // check if piece is on new spot
             ChessPosition newPosition = new ChessPosition(newRow, newCol);
             ChessPiece checkPiece = board.getPiece(newPosition);
-            // check if spot is empty, or other team's piece
-            if (checkPiece == null || checkPiece.getTeamColor() != color) {
+
+            // if attack mode and the spot is taken
+            if (attack && checkPiece != null) {
+                // if piece is opposite color, add the move
+                if (checkPiece.getTeamColor() != color) {
+                    addedMoves.add(new ChessMove(position, newPosition, null));
+                }
+            }
+            // if not attack mode, and spot is empty, add the move
+            if (!attack && checkPiece == null) {
                 addedMoves.add(new ChessMove(position, newPosition, null));
             }
         }

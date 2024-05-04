9102669e73d7e8cad4ead6fc77f996024756d691
n8petersen <n8knows@gmail.com>
2024-05-04 17:24:03 -0600

    add knight moves

--- shared/src/main/java/chess/moves/KnightMoves.java
+++ shared/src/main/java/chess/moves/KnightMoves.java
@@ -1,8 +1,6 @@
 package chess.moves;
 
-import chess.ChessBoard;
-import chess.ChessMove;
-import chess.ChessPosition;
+import chess.*;
 
 import java.util.ArrayList;
 
@@ -16,6 +14,38 @@ public class KnightMoves {
         //          Just check for edge of board, and for own pieces for invalid moves to NOT add
         //          Any moves that are in the board with null pieces, or opposite color pieces add as valid moves
 
+        moveList.addAll(addMoves(board, myPosition, 2,-1));
+        moveList.addAll(addMoves(board, myPosition, 2,1));
+
+        moveList.addAll(addMoves(board, myPosition, 1,2));
+        moveList.addAll(addMoves(board, myPosition, -1,2));
+
+        moveList.addAll(addMoves(board, myPosition, -2,-1));
+        moveList.addAll(addMoves(board, myPosition, -2,1));
+
+        moveList.addAll(addMoves(board, myPosition, 1,-2));
+        moveList.addAll(addMoves(board, myPosition, -1,-2));
+
+
         return moveList;
+    }
+
+    private ArrayList<ChessMove> addMoves(ChessBoard board, ChessPosition position, int moveRow, int moveCol) {
+        ArrayList<ChessMove> addedMoves = new ArrayList<ChessMove>();
+        ChessGame.TeamColor color = board.getPiece(position).getTeamColor();
+        int newRow = position.getRow() + moveRow;
+        int newCol = position.getColumn() + moveCol;
+
+        // check if out of edge of board
+        if (newRow < 9 && newRow > 0 && newCol < 9 && newCol > 0) {
+            // check if piece is on new spot
+            ChessPosition newPosition = new ChessPosition(newRow, newCol);
+            ChessPiece checkPiece = board.getPiece(newPosition);
+            // check if spot is empty, or other team's piece
+            if (checkPiece == null || checkPiece.getTeamColor() != color) {
+                addedMoves.add(new ChessMove(position, newPosition, null));
+            }
+        }
+        return addedMoves;
     }
 }

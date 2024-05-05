5ff9d198511648256d6a8249ff583620540170b0
n8petersen <n8knows@gmail.com>
2024-05-04 18:29:29 -0600

    add rook moves

--- shared/src/main/java/chess/moves/RookMoves.java
+++ shared/src/main/java/chess/moves/RookMoves.java
@@ -1,8 +1,6 @@
 package chess.moves;
 
-import chess.ChessBoard;
-import chess.ChessMove;
-import chess.ChessPosition;
+import chess.*;
 
 import java.util.ArrayList;
 
@@ -17,6 +15,34 @@ public class RookMoves {
         //          If own color, don't add the move and stop loop
         //          If other color, add move and stop loop
 
+        moveList.addAll(addMoves(board, myPosition, 1,0));
+        moveList.addAll(addMoves(board, myPosition, -1,0));
+        moveList.addAll(addMoves(board, myPosition, 0,-1));
+        moveList.addAll(addMoves(board, myPosition, 0,1));
+
         return moveList;
+    }
+
+    private ArrayList<ChessMove> addMoves(ChessBoard board, ChessPosition position, int moveRow, int moveCol) {
+        ArrayList<ChessMove> addedMoves = new ArrayList<ChessMove>();
+        ChessGame.TeamColor color = board.getPiece(position).getTeamColor();
+        int newRow = position.getRow() + moveRow;
+        int newCol = position.getColumn() + moveCol;
+
+        // loop while still in board
+        while (newRow < 9 && newRow > 0 && newCol < 9 && newCol > 0) {
+            ChessPosition newPosition = new ChessPosition(newRow, newCol);
+            ChessPiece checkPiece = board.getPiece(newPosition);
+            if (checkPiece == null || checkPiece.getTeamColor() != color) {
+                addedMoves.add(new ChessMove(position, newPosition, null));
+                newRow += moveRow;
+                newCol += moveCol;
+            }
+            if (checkPiece != null) {
+                break;
+            }
+        }
+
+        return addedMoves;
     }
 }

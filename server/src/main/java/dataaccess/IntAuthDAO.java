d294af342a97c27d178b3bd935e29ff57a9b629c
n8petersen <n8knows@gmail.com>
2024-05-22 22:37:29 -0600

    Fix Auth DAO Interface

--- server/src/main/java/dataaccess/IntAuthDAO.java
+++ server/src/main/java/dataaccess/IntAuthDAO.java
@@ -2,7 +2,7 @@ package dataaccess;
 
 import model.AuthData;
 
-public interface AuthDAO {
+public interface IntAuthDAO {
     AuthData createAuth(String username) throws DataAccessException;
     AuthData readAuth(String authToken) throws DataAccessException;
     void deleteAuth(String authToken) throws DataAccessException;

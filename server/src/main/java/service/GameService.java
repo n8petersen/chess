2a01657a6aaf9b88f644252a3bdd514925717480
n8petersen <n8knows@gmail.com>
2024-05-27 13:16:50 -0600

    Filled out GameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,11 +1,62 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.IntGameDAO;
+import model.GameData;
+
+import java.util.Collection;
+
 public class GameService {
-    // TODO: Logic to CRUD GameData
-    //  createGame()
-    //  getGame()
-    //  listGames()
-    //  updateGame()
-    //  deleteGame()
-    //  clearGames()
+
+    private IntGameDAO gameDataAccess;
+
+    private GameService() {}
+
+    public int createGame(String gameName) {
+        try {
+            return gameDataAccess.createGame(gameName);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public GameData getGame(int gameId) {
+        try {
+            return gameDataAccess.readGame(gameId);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Collection<GameData> listGames() {
+        try {
+            return gameDataAccess.readAllGames();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateGame(GameData game) {
+        try {
+            gameDataAccess.updateGame(game);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteDame(int gameId) {
+        try {
+            gameDataAccess.deleteGame(gameId);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void clearGames() {
+        try {
+            gameDataAccess.clear();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }

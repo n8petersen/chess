f68775afd34ffd72571c766835b2c17a2d8f7cab
n8petersen <n8knows@gmail.com>
2024-05-27 13:11:18 -0600

    Filled out AuthService

--- server/src/main/java/service/AuthService.java
+++ server/src/main/java/service/AuthService.java
@@ -1,9 +1,45 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.IntAuthDAO;
+import model.AuthData;
+import model.UserData;
+
 public class AuthService {
-    // TODO: Logic to CRUD AuthData
-    //  createAuth()
-    //  getAuth()
-    //  deleteAuth()
-    //  clearAuths()
+
+    private IntAuthDAO authDataAccess;
+
+    private AuthService() {}
+
+    public AuthData createAuth(UserData user) {
+        try {
+            return authDataAccess.createAuth(user.username());
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public AuthData getAuth(String authToken) {
+        try {
+            return authDataAccess.readAuth(authToken);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteAuth(String authToken) {
+        try {
+            authDataAccess.deleteAuth(authToken);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void clearAuths() {
+        try {
+            authDataAccess.clear();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }

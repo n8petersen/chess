641ba807afcf2eb97639b34dddaa6c00ca0f97d0
n8petersen <n8knows@gmail.com>
2024-05-27 16:15:16 -0600

    add barebones endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,9 +2,8 @@ package server;
 
 import com.google.gson.Gson;
 import dataaccess.*;
-import service.AuthService;
-import service.GameService;
-import service.UserService;
+import service.*;
+import model.*;
 import java.util.Map;
 import spark.*;
 
@@ -18,23 +17,25 @@ public class Server {
     private final Gson serializer = new Gson();
 
     public Server() {
-        var authDao = new MemAuthDAO();
-        var gameDao = new MemGameDAO();
-        var userDao = new MemUserDAO();
+        MemAuthDAO authDao = new MemAuthDAO();
+        MemGameDAO gameDao = new MemGameDAO();
+        MemUserDAO userDao = new MemUserDAO();
 
         this.authService = new AuthService(authDao);
+        this.gameService = new GameService(gameDao);
+        this.userService = new UserService(userDao);
     }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
         Spark.staticFiles.location("web");
 
-//        Spark.post("/user", this::register);
-//        Spark.post("/session". this::login);
-//        Spark.delete("/session", this::logout);
-//        Spark.get("/game", this::listGames);
-//        Spark.post("/game", this::createGame);
-//        Spark.put("/game", this::joinGame);
+        Spark.post("/user", this::register);
+        Spark.post("/session", this::login);
+        Spark.delete("/session", this::logout);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game", this::createGame);
+        Spark.put("/game", this::joinGame);
         Spark.delete("/db", this::clear);
 
         Spark.awaitInitialization();
@@ -53,19 +54,61 @@ public class Server {
         Spark.awaitStop();
     }
 
+    private Object register(Request req, Response res) {
+        System.out.println("Register user");
+        System.out.println(req.body());
+        UserData newUser = serializer.fromJson(req.body(), UserData.class);
+
+        return new Object();
+    }
+
+    private Object login(Request req, Response res) {
+        System.out.println("Login user");
+        System.out.println(req);
+        return new Object();
+    }
+
+    private Object logout(Request req, Response res) {
+        System.out.println("Logout user");
+        System.out.println(req);
+        return new Object();
+    }
+
+    private Object listGames(Request req, Response res) {
+        System.out.println("List Games");
+        System.out.println(req);
+        return new Object();
+    }
+
+    private Object createGame(Request req, Response res) {
+        System.out.println("Create game");
+        System.out.println(req);
+        return new Object();
+    }
+
+    private Object joinGame(Request req, Response res) {
+        System.out.println("Join Game");
+        System.out.println(req);
+        return new Object();
+    }
+
     private Object clear(Request req, Response res) {
-        res.type("application/json");
-        try {
-            authService.clearAuths();
-            gameService.clearGames();
-            userService.clearUsers();
-            res.status(200);
-            return "";
-        } catch (DataAccessException e) {
-            return Error(e, req, res);
-        }
-        catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+//        res.type("application/json");
+//        try {
+//            authService.clearAuths();
+//            gameService.clearGames();
+//            userService.clearUsers();
+//            res.status(200);
+//            return "";
+//        } catch (DataAccessException e) {
+//            return Error(e, req, res);
+//        }
+//        catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+
+        System.out.println("Clear App");
+        System.out.println(req);
+        return new Object();
     }
 }

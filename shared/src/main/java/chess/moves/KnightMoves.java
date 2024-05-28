942f03662fec2519978d9379273c11af2462dbdb
npetersen <npetersen@goldpointsystems.com>
2024-05-28 13:34:34 -0600

    move common special moves into separate class

--- shared/src/main/java/chess/moves/KnightMoves.java
+++ shared/src/main/java/chess/moves/KnightMoves.java
@@ -21,35 +21,16 @@ public class KnightMoves {
         //      | | | | | | | | |
         //      -----------------
 
-        moveList.addAll(addMoves(board, myPosition, 2,-1));
-        moveList.addAll(addMoves(board, myPosition, 2,1));
-        moveList.addAll(addMoves(board, myPosition, 1,2));
-        moveList.addAll(addMoves(board, myPosition, -1,2));
-        moveList.addAll(addMoves(board, myPosition, -2,-1));
-        moveList.addAll(addMoves(board, myPosition, -2,1));
-        moveList.addAll(addMoves(board, myPosition, 1,-2));
-        moveList.addAll(addMoves(board, myPosition, -1,-2));
-
+        moveList.addAll(new MoveAdder().addSpecialMoves(board, myPosition, 2,-1));
+        moveList.addAll(new MoveAdder().addSpecialMoves(board, myPosition, 2,1));
+        moveList.addAll(new MoveAdder().addSpecialMoves(board, myPosition, 1,2));
+        moveList.addAll(new MoveAdder().addSpecialMoves(board, myPosition, -1,2));
+        moveList.addAll(new MoveAdder().addSpecialMoves(board, myPosition, -2,-1));
+        moveList.addAll(new MoveAdder().addSpecialMoves(board, myPosition, -2,1));
+        moveList.addAll(new MoveAdder().addSpecialMoves(board, myPosition, 1,-2));
+        moveList.addAll(new MoveAdder().addSpecialMoves(board, myPosition, -1,-2));
 
         return moveList;
     }
 
-    private ArrayList<ChessMove> addMoves(ChessBoard board, ChessPosition position, int moveRow, int moveCol) {
-        ArrayList<ChessMove> addedMoves = new ArrayList<>();
-        ChessGame.TeamColor color = board.getPiece(position).getTeamColor();
-        int newRow = position.getRow() + moveRow;
-        int newCol = position.getColumn() + moveCol;
-
-        // check if out of edge of board
-        if (newRow < 9 && newRow > 0 && newCol < 9 && newCol > 0) {
-            // check if piece is on new spot
-            ChessPosition newPosition = new ChessPosition(newRow, newCol);
-            ChessPiece checkPiece = board.getPiece(newPosition);
-            // check if spot is empty, or other team's piece
-            if (checkPiece == null || checkPiece.getTeamColor() != color) {
-                addedMoves.add(new ChessMove(position, newPosition, null));
-            }
-        }
-        return addedMoves;
-    }
 }
--- shared/src/main/java/chess/moves/MoveAdder.java
+++ shared/src/main/java/chess/moves/MoveAdder.java
@@ -28,4 +28,23 @@ public class MoveAdder {
 
         return addedMoves;
     }
+
+    public ArrayList<ChessMove> addSpecialMoves(ChessBoard board, ChessPosition position, int moveRow, int moveCol) {
+        ArrayList<ChessMove> addedMoves = new ArrayList<>();
+        ChessGame.TeamColor color = board.getPiece(position).getTeamColor();
+        int newRow = position.getRow() + moveRow;
+        int newCol = position.getColumn() + moveCol;
+
+        // check if out of edge of board
+        if (newRow < 9 && newRow > 0 && newCol < 9 && newCol > 0) {
+            // check if piece is on new spot
+            ChessPosition newPosition = new ChessPosition(newRow, newCol);
+            ChessPiece checkPiece = board.getPiece(newPosition);
+            // check if spot is empty, or other team's piece
+            if (checkPiece == null || checkPiece.getTeamColor() != color) {
+                addedMoves.add(new ChessMove(position, newPosition, null));
+            }
+        }
+        return addedMoves;
+    }
 }

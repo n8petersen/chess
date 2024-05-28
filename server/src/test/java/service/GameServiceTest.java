5a676170af36ccd416f8ea562f2d9e47be545552
npetersen <npetersen@goldpointsystems.com>
2024-05-28 13:22:17 -0600

    add service test classes

---
+++ server/src/test/java/service/GameServiceTest.java
@@ -0,0 +1,30 @@
+package service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceTest {
+
+    @Test
+    @DisplayName("Create Game")
+    void createNewGame() {
+//        Assertions.
+    }
+
+    @Test
+    @DisplayName("List Games")
+    void listGames() {
+    }
+
+    @Test
+    @DisplayName("Join Game")
+    void joinGame() {
+    }
+
+    @Test
+    @DisplayName("Clear Games")
+    void clearGames() {
+    }
+}
---
+++ server/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,28 @@
+package service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    @DisplayName("Create User")
+    void createUser() {
+    }
+
+    @Test
+    @DisplayName("Login User")
+    void loginUser() {
+    }
+
+    @Test@DisplayName("Logout User")
+    void logoutUser() {
+    }
+
+    @Test
+    @DisplayName("Clear User Auths")
+    void clearUsersAuths() {
+    }
+}

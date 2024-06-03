6c49cd3ab2f0fefe73855833d4d8e3a767555f56
n8petersen <n8knows@gmail.com>
2024-06-03 13:27:01 -0600

    add create, read, clear

--- server/src/main/java/dataaccess/SqlUserDAO.java
+++ server/src/main/java/dataaccess/SqlUserDAO.java
@@ -5,8 +5,6 @@ import model.UserData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import static java.sql.Types.NULL;
-
 public class SqlUserDAO implements IntUserDAO {
 
     public SqlUserDAO() {
@@ -14,46 +12,47 @@ public class SqlUserDAO implements IntUserDAO {
     }
 
     public void createUser(UserData user) throws DataAccessException {
-        String statement = "INSERT INTO `chess`.`user` (`username`, `email`, `password`) VALUES (?, ?, ?)";
-        try {
-            executeSql(statement, user.username(), user.email(), user.password());
-        } catch (BadRequestException e) {
-            throw new RuntimeException(e);
+        try (var conn = DatabaseManager.getConnection()) {
+            String statement = "INSERT INTO `chess`.`user` (`username`, `email`, `password`) VALUES (?, ?, ?)";
+            try (var ps = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
+                ps.setString(1, user.username());
+                ps.setString(2, user.email());
+                ps.setString(3, user.password());
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to add user to database: %s", e.getMessage()));
         }
     }
 
-    public UserData readUser(String username) {
+    public UserData readUser(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            String statement = "SELECT * FROM `chess`.`user` WHERE `username` = ?";
+            try (var ps = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
+                ps.setString(1, username);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        String user = rs.getString("username");
+                        String email = rs.getString("email");
+                        String password = rs.getString("password");
+                        return new UserData(user, email, password);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
     public void clear() throws DataAccessException {
-
-    }
-
-    private int executeSql(String statement, Object... params) throws DataAccessException, BadRequestException {
         try (var conn = DatabaseManager.getConnection()) {
+            String statement = "TRUNCATE `chess`.`user`;";
             try (var ps = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                    switch (param) {
-                        case String p -> ps.setString(i + 1, p);
-                        case Integer p -> ps.setInt(i + 1, p);
-                        case null -> ps.setInt(i + 1, NULL);
-                        default -> {
-                        }
-                    }
-                }
                 ps.executeUpdate();
-
-                var rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                    return rs.getInt(1);
-                }
-
-                return 0;
             }
         } catch (SQLException e) {
-            throw new BadRequestException(String.format("Unable to configure database: %s", e.getMessage()));
+            throw new DataAccessException(String.format("Unable to add user to database: %s", e.getMessage()));
         }
     }
 
@@ -63,11 +62,10 @@ public class SqlUserDAO implements IntUserDAO {
               `id` int NOT NULL AUTO_INCREMENT,
               `username` varchar(45) NOT NULL,
               `email` varchar(45) DEFAULT NULL,
-              `password` varchar(45) DEFAULT NULL,
+              `password` varchar(100) DEFAULT NULL,
               PRIMARY KEY (`id`,`username`),
               UNIQUE KEY `username_UNIQUE` (`username`),
-              UNIQUE KEY `id_UNIQUE` (`id`),
-              UNIQUE KEY `email_UNIQUE` (`email`)
+              UNIQUE KEY `id_UNIQUE` (`id`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
             """
     };

0cf477ca65f1ddc09260d793ca1d4ecae084426f
n8petersen <n8knows@gmail.com>
2024-06-03 21:41:30 -0600

    clarify types

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -3,7 +3,7 @@ import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessPiece piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
 
         int portNum = 8080;

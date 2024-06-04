62bc169bce2263d5957eb91f11105dd317ef5569
n8petersen <n8knows@gmail.com>
2024-06-03 21:37:33 -0600

    create and read game

--- server/src/main/java/dataaccess/SqlGameDAO.java
+++ server/src/main/java/dataaccess/SqlGameDAO.java
@@ -1,23 +1,51 @@
 package dataaccess;
 
+import chess.ChessGame;
 import model.GameData;
+import com.google.gson.Gson;
 
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Collection;
 import java.util.List;
 
 public class SqlGameDAO implements IntGameDAO {
+
+    private final Gson serializer = new Gson();
 
     public SqlGameDAO() {
         configureDb();
     }
 
     public GameData createGame(String gameName) throws DataAccessException {
-        return null;
+        ChessGame game = new ChessGame();
+        String gameJson = serializer.toJson(game);
+        int id = new SQLExecutor().updateQuery("INSERT INTO `chess`.`game` (`gameName`, `whiteUsername`, `blackUsername`, `gameData`) VALUES (?, ?, ?, ?)",
+                gameName,
+                null,
+                null,
+                gameJson);
+        return new GameData(id, gameName, null, null, game);
     }
 
     public GameData readGame(int gameId) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String statement = "SELECT * FROM `chess`.`game` WHERE `id` = ?";
+            try (PreparedStatement ps = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
+                ps.setInt(1, gameId);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        String gameName = rs.getString("gameName");
+                        String whiteUsername = rs.getString("whiteUsername");
+                        String blackUsername = rs.getString("blackUsername");
+                        String gameJson = rs.getString("GameData");
+                        ChessGame game = serializer.fromJson(gameJson, ChessGame.class);
+                        return new GameData(gameId, gameName, whiteUsername, blackUsername, game);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
@@ -47,6 +75,7 @@ public class SqlGameDAO implements IntGameDAO {
             `gameName` varchar(45) DEFAULT NULL,
             `whiteUsername` varchar(45) DEFAULT NULL,
             `blackUsername` varchar(45) DEFAULT NULL,
+            `gameData` json DEFAULT NULL,
             PRIMARY KEY (`id`),
             UNIQUE KEY `id_UNIQUE` (`id`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;

33f26c7600fec923f4bcfeb3a5dd005691fae86f
n8petersen <n8knows@gmail.com>
2024-06-03 23:13:52 -0600

    add unit tests stubs

---
+++ server/src/test/java/database/SqlGameTest.java
@@ -0,0 +1,43 @@
+package database;
+
+import org.junit.jupiter.api.Test;
+
+
+class SqlGameTest {
+
+    @Test
+    void createGameGood() {
+    }
+
+    @Test
+    void createGameBad() {
+    }
+
+    @Test
+    void readGameGood() {
+    }
+
+    @Test
+    void readGameBad() {
+    }
+
+    @Test
+    void readAllGamesGood() {
+    }
+
+    @Test
+    void readAllGamesBad() {
+    }
+
+    @Test
+    void updateGameGood() {
+    }
+
+    @Test
+    void updateGameBad() {
+    }
+
+    @Test
+    void clear() {
+    }
+}
---
+++ server/src/test/java/database/SqlUserTest.java
@@ -0,0 +1,26 @@
+package database;
+
+import org.junit.jupiter.api.Test;
+
+class SqlUserTest {
+
+    @Test
+    void createUserGood() {
+    }
+
+    @Test
+    void createUserBad() {
+    }
+
+    @Test
+    void readUserGood() {
+    }
+
+    @Test
+    void readUserBad() {
+    }
+
+    @Test
+    void clear() {
+    }
+}

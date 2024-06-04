031de768dee7bffda04c3145a2dfa11834b17c23
n8petersen <n8knows@gmail.com>
2024-06-04 11:20:11 -0600

    setup user test

--- server/src/test/java/dataaccess/SqlUserDAOTest.java
+++ server/src/test/java/dataaccess/SqlUserDAOTest.java
@@ -1,9 +1,19 @@
 package dataaccess;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class SqlUserDAOTest {
+
+    private IntUserDAO userDao;
+
+    @BeforeEach
+    public void setup() throws DataAccessException {
+        (userDao = new SqlUserDAO()).clear();
+    }
 
     @Test
     void createUserGood() {

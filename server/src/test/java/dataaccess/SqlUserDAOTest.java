d95e133ffce72c6a9552fef34a754970d768f897
n8petersen <n8knows@gmail.com>
2024-06-04 16:08:30 -0600

    SQL User Unit Tests

--- server/src/test/java/dataaccess/SqlUserDAOTest.java
+++ server/src/test/java/dataaccess/SqlUserDAOTest.java
@@ -1,7 +1,9 @@
 package dataaccess;
 
+import model.UserData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mindrot.jbcrypt.BCrypt;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,22 +18,44 @@ class SqlUserDAOTest {
     }
 
     @Test
-    void createNewUser() {
+    void createNewUser() throws DataAccessException {
+        UserData testUser = new UserData("user", "pass", "email");
+        UserData insertUser = userDao.createUser(testUser);
+        assertTrue(BCrypt.checkpw(testUser.password(), insertUser.password()));
     }
 
     @Test
     void createUserTaken() {
+        assertThrows(DataAccessException.class,
+                () -> {
+                    userDao.createUser(new UserData("user", "pass", "email"));
+                    userDao.createUser(new UserData("user", "pass", "email"));
+                }
+        );
     }
 
     @Test
-    void readUser() {
+    void readUser() throws DataAccessException {
+        UserData testUser = new UserData("user", "pass", "email");
+        userDao.createUser(testUser);
+        UserData readUser = userDao.readUser("user");
+        assertTrue(BCrypt.checkpw(testUser.password(), readUser.password()));
     }
 
     @Test
-    void readUserNullUsername() {
+    void readUserNullUsername() throws DataAccessException {
+        UserData userData = userDao.readUser(null);
+        assertNull(userData);
     }
 
     @Test
-    void clear() {
+    void clear() throws DataAccessException {
+        userDao.createUser(new UserData("user1", "pass", "user1"));
+        userDao.createUser(new UserData("user2", "pass", "user2"));
+        userDao.createUser(new UserData("user3", "pass", "user3"));
+        userDao.clear();
+        assertNull(userDao.readUser("user1"));
+        assertNull(userDao.readUser("user2"));
+        assertNull(userDao.readUser("user3"));
     }
 }

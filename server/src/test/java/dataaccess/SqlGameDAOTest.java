0c88af9f670179d7d638ec879f98825e2f4dffda
n8petersen <n8knows@gmail.com>
2024-06-04 11:09:23 -0600

    Sql Game Tests complete

--- server/src/test/java/dataaccess/SqlGameDAOTest.java
+++ server/src/test/java/dataaccess/SqlGameDAOTest.java
@@ -1,42 +1,100 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.GameData;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class SqlGameDAOTest {
 
-    @Test
-    void createGameGood() {
+    private IntGameDAO gameDao;
+
+    @BeforeEach
+    public void setup() throws DataAccessException {
+        (gameDao = new SqlGameDAO()).clear();
     }
 
     @Test
-    void createGameBad() {
+    void createGameGood() throws DataAccessException {
+        GameData testGame = new GameData(1, null, null, "testName", new ChessGame());
+        GameData insertGame = gameDao.createGame("testName");
+        assertEquals(testGame, insertGame);
+        assertNotEquals(2, testGame.gameID());
     }
 
     @Test
-    void readGameGood() {
+    void createGameNullName() {
+        assertThrows(DataAccessException.class,
+                () -> gameDao.createGame(null)
+        );
     }
 
     @Test
-    void readGameBad() {
+    void readGameGood() throws DataAccessException {
+        GameData insertGame = gameDao.createGame("testName");
+        GameData readGame = gameDao.readGame(insertGame.gameID());
+        assertEquals(readGame, insertGame);
     }
 
     @Test
-    void readAllGamesGood() {
+    void readGameWrongID() throws DataAccessException {
+        GameData insertGame = gameDao.createGame("testName");
+        GameData readGame = gameDao.readGame(2);
+        assertNotEquals(readGame, insertGame);
+        assertNull(readGame);
     }
 
     @Test
-    void readAllGamesBad() {
+    void readAllGamesAfterInserts() throws DataAccessException {
+        GameData insertGame1 = gameDao.createGame("one");
+        GameData insertGame2 = gameDao.createGame("two");
+        GameData insertGame3 = gameDao.createGame("three");
+        Collection<GameData> readGames = gameDao.readAllGames();
+        assertTrue(readGames.contains(insertGame1));
+        assertTrue(readGames.contains(insertGame2));
+        assertTrue(readGames.contains(insertGame3));
     }
 
     @Test
-    void updateGameGood() {
+    void readAllGamesEmpty() throws DataAccessException {
+        Collection<GameData> readGames = gameDao.readAllGames();
+        assertTrue(readGames.isEmpty());
     }
 
     @Test
-    void updateGameBad() {
+    void updateGameGood() throws DataAccessException {
+        GameData insertNewGame = gameDao.createGame("testName");
+        GameData updateGameData = new GameData(1, "white", "black", "testName", new ChessGame());
+        gameDao.updateGame(updateGameData);
+        GameData getGame = gameDao.readGame(1);
+        assertNotEquals(updateGameData, insertNewGame);
+        assertEquals(updateGameData, getGame);
     }
 
     @Test
-    void clear() {
+    void updateGameNull() {
+        assertThrows(DataAccessException.class,
+                () -> {
+                    gameDao.createGame("testName");
+                    gameDao.updateGame(new GameData(1, "white", "black", "testName", null));
+                }
+        );
+    }
+
+    @Test
+    void clear() throws DataAccessException {
+        GameData insert1 = gameDao.createGame("game1");
+        GameData insert2 = gameDao.createGame("game2");
+        GameData insert3 = gameDao.createGame("game3");
+        gameDao.clear();
+        Collection<GameData> readGames = gameDao.readAllGames();
+        assertFalse(readGames.contains(insert1));
+        assertFalse(readGames.contains(insert2));
+        assertFalse(readGames.contains(insert3));
+        assertTrue(readGames.isEmpty());
     }
 }

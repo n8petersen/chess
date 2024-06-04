c756e452a414491e0592508f5a2e18875dc39276
n8petersen <n8knows@gmail.com>
2024-06-03 18:37:58 -0600

    create clear table

--- server/src/main/java/dataaccess/SqlGameDAO.java
+++ server/src/main/java/dataaccess/SqlGameDAO.java
@@ -2,6 +2,8 @@ package dataaccess;
 
 import model.GameData;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 import java.util.List;
 
@@ -28,10 +30,47 @@ public class SqlGameDAO implements IntGameDAO {
     }
 
     public void clear() throws DataAccessException {
-
+        try (var conn = DatabaseManager.getConnection()) {
+            String statement = "TRUNCATE `chess`.`game`;";
+            try (var ps = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to add user to database: %s", e.getMessage()));
+        }
     }
 
-    private void configureDb() {
+    private final String[] createTable = {
+            """
+            CREATE TABLE IF NOT EXISTS `game` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `gameName` varchar(45) DEFAULT NULL,
+            `whiteUsername` varchar(45) DEFAULT NULL,
+            `blackUsername` varchar(45) DEFAULT NULL,
+            PRIMARY KEY (`id`),
+            UNIQUE KEY `id_UNIQUE` (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+            """
+    };
 
+    private void configureDb() {
+        try {
+            DatabaseManager.createDatabase();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        try (java.sql.Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createTable) {
+                try (java.sql.PreparedStatement preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException | DataAccessException e) {
+            try {
+                throw new BadRequestException(String.format("Unable to configure database: %s", e.getMessage()));
+            } catch (BadRequestException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
     }
 }

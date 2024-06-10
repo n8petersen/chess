1e5b4972af6bc7e387b6902fe16bc7fff797b6e3
n8petersen <n8knows@gmail.com>
2024-06-09 21:35:57 -0600

    add register, login, logout

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,17 +1,25 @@
 package ui;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import static ui.EscapeSequences.*;
+import static ui.State.*;
 
 public class ChessClient {
 
+    private static final Logger log = LoggerFactory.getLogger(ChessClient.class);
     final ServerFacade server;
     private State state = State.LOGGED_OUT;
-    private String auth;
+    private String authToken;
 
     public ChessClient(String host, int port) {
         server = new ServerFacade(host, port);
     }
 
-    public String readInput(String input) {
+    public String readInput(String input) throws Exception {
         var result = "Couldn't process command: " + input + ". Try running 'Help'";
         input = input.toLowerCase();
         var input_array = input.split(" ");
@@ -19,8 +27,43 @@ public class ChessClient {
         return switch (input_array[0]) {
             case "help" -> help();
             case "quit" -> quit();
+            case "register" -> register(input_array);
+            case "logout" -> logout();
+            case "login" -> login(input_array);
             default -> result;
         };
+    }
+
+    private String register(String[] param) throws Exception {
+        String result = "Couldn't register. Try again";
+        if (param.length == 4 && state == LOGGED_OUT) {
+            var resp = server.register(param[1], param[2], param[3]);
+            authToken = resp.authToken();
+            state = LOGGED_IN;
+            result = "Created and logged in as " + param[1];
+        }
+        return result;
+    }
+
+    private String login(String[] param) throws Exception {
+        String result = "";
+        if (param.length == 3 && state == LOGGED_OUT) {
+            var resp = server.login(param[1], param[2]);
+            authToken = resp.authToken();
+            state = LOGGED_IN;
+            result = "Logged in as " + param[1];
+        }
+        return result;
+    }
+
+    private String logout() throws Exception {
+        String result = "Couldn't logout.";
+        if (state == LOGGED_IN) {
+            server.logout(authToken);
+            state = LOGGED_OUT;
+            result = "Logged out.";
+        }
+        return result;
     }
 
     private String help() {
--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,25 +1,70 @@
 package ui;
 
 import chess.ChessGame;
+import com.google.gson.Gson;
 import model.AuthData;
 import model.GameData;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.Map;
+
 public class ServerFacade {
 
-    public AuthData register(String username, String password, String email) {
+    private final String hostname;
+    private final Gson serializer = new Gson();
 
-
-        return null;
+    public ServerFacade(String host, int port) {
+        hostname = String.format("http://%s:%d", host, port);
     }
 
-    public AuthData login(String username, String password) {
-
-
-        return null;
+    public AuthData register(String username, String password, String email) throws Exception {
+        AuthData auth = null;
+        URL url = (new URI(hostname + "/user")).toURL();
+        var req = Map.of("username", username, "password", password, "email", email);
+        var reqJson = serializer.toJson(req);
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        http.addRequestProperty("Accept", "application/json");
+        var outStream = http.getOutputStream();
+        outStream.write(reqJson.getBytes());
+        http.connect();
+        try (InputStream respBody = http.getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(respBody);
+            auth = serializer.fromJson(inputStreamReader, AuthData.class);
+        }
+        return auth;
     }
 
-    public void logout(String authToken) {
+    public AuthData login(String username, String password) throws Exception {
+        AuthData auth = null;
+        URL url = (new URI(hostname + "/session")).toURL();
+        var req = Map.of("username", username, "password", password);
+        var reqJson = serializer.toJson(req);
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        http.addRequestProperty("Accept", "application/json");
+        var outStream = http.getOutputStream();
+        outStream.write(reqJson.getBytes());
+        http.connect();
+        try (InputStream respBody = http.getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(respBody);
+            auth = serializer.fromJson(inputStreamReader, AuthData.class);
+        }
+        return auth;
+    }
 
+    public void logout(String authToken) throws Exception {
+        URL url = (new URI(hostname + "/session")).toURL();
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod("DELETE");
+        http.addRequestProperty("authorization", authToken);
+        http.connect();
+        InputStream respBody = http.getInputStream();
     }
 
     public GameData createGame(String authToken, String gameName) {

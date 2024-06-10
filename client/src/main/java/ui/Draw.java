b6d4092e2476917b9a336464add630252e95211d
n8petersen <n8knows@gmail.com>
2024-06-10 12:21:00 -0600

    work on drawing

--- client/src/main/java/ui/Draw.java
+++ client/src/main/java/ui/Draw.java
@@ -23,50 +23,88 @@ public class Draw {
         return "";
     }
 
-    private void drawRow(String[] items, String color, boolean whiteOrientation) {
+    private void drawMiddleRow(String[] items, String initialColor, boolean whiteOrientation) {
+        String color = initialColor;
         if (!whiteOrientation) {
-            for (int i = 0; i < 8; i++) {
-                System.out.print(color);
-                System.out.print(items[i]);
-                color = (color.equals(SET_BG_COLOR_DARK_GREY) ? SET_BG_COLOR_LIGHT_GREY : SET_BG_COLOR_DARK_GREY);
+            for (int i = 0; i < 10; i++) {
+                if (i == 0 || i == 9) {
+                    color = SET_BG_COLOR_DARK_GREY;
+                    System.out.print(color);
+                    System.out.print(items[i]);
+                    color = initialColor;
+                } else {
+                    System.out.print(color);
+                    System.out.print(items[i]);
+                    color = (color.equals(SET_BG_COLOR_BLACK) ? SET_BG_COLOR_LIGHT_GREY : SET_BG_COLOR_BLACK);
+                }
             }
         } else {
-            for (int i = 7; i >= 0; i--) {
-                System.out.print(color);
+            for (int i = 9; i >= 0; i--) {
+                if (i == 0 || i == 9) {
+                    color = SET_BG_COLOR_DARK_GREY;
+                    System.out.print(color);
+                    System.out.print(items[i]);
+                    color = initialColor;
+                } else {
+                    System.out.print(color);
+                    System.out.print(items[i]);
+                    color = (color.equals(SET_BG_COLOR_BLACK) ? SET_BG_COLOR_LIGHT_GREY : SET_BG_COLOR_BLACK);
+                }
+            }
+        }
+        System.out.print(RESET_BG_COLOR + "\n");
+    }
+
+    private void drawEdgeRow(String[] items, boolean whiteOrientation) {
+        if (!whiteOrientation) {
+            for (int i = 0; i < 10; i++) {
+                System.out.print(SET_BG_COLOR_DARK_GREY);
                 System.out.print(items[i]);
-                color = (color.equals(SET_BG_COLOR_DARK_GREY) ? SET_BG_COLOR_LIGHT_GREY : SET_BG_COLOR_DARK_GREY);
+            }
+        } else {
+            for (int i = 9; i >= 0; i--) {
+                System.out.print(SET_BG_COLOR_DARK_GREY);
+                System.out.print(items[i]);
             }
         }
         System.out.print(RESET_BG_COLOR + "\n");
     }
 
     private void drawStarter() {
-        String[] whiteRow1 = {WHITE_ROOK,WHITE_KNIGHT,WHITE_BISHOP,WHITE_KING,WHITE_QUEEN,WHITE_BISHOP,WHITE_KNIGHT,WHITE_ROOK};
-        String[] whiteRow2 = {WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN};
-        String[] rowMid = {EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY};
-        String[] blackRow2 = {BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN};
-        String[] blackRow1 = {BLACK_ROOK,BLACK_KNIGHT,BLACK_BISHOP,BLACK_KING,BLACK_QUEEN,BLACK_BISHOP,BLACK_KNIGHT,BLACK_ROOK};
+        String[] edgeRow = {EMPTY," h ","  g ", " f ", "  e ", " d ", "  c "," b  "," a ", EMPTY};
+        String[] whiteRow1 = {" 1 ",WHITE_ROOK,WHITE_KNIGHT,WHITE_BISHOP,WHITE_KING,WHITE_QUEEN,WHITE_BISHOP,WHITE_KNIGHT,WHITE_ROOK," 1 "};
+        String[] whiteRow2 = {" 2 ",WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN," 2 "};
+        String[] rowMid1 = {" 3 ",EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY," 3 "};
+        String[] rowMid2 = {" 4 ",EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY," 4 "};
+        String[] rowMid3 = {" 5 ",EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY," 5 "};
+        String[] rowMid4 = {" 6 ",EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY," 6 "};
+        String[] blackRow2 = {" 7 ",BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN," 7 "};
+        String[] blackRow1 = {" 8 ",BLACK_ROOK,BLACK_KNIGHT,BLACK_BISHOP,BLACK_KING,BLACK_QUEEN,BLACK_BISHOP,BLACK_KNIGHT,BLACK_ROOK," 8 "};
 
         // black down
-        drawRow(whiteRow1, SET_BG_COLOR_LIGHT_GREY, false);
-        drawRow(whiteRow2, SET_BG_COLOR_DARK_GREY, false);
-        drawRow(rowMid, SET_BG_COLOR_LIGHT_GREY, false);
-        drawRow(rowMid, SET_BG_COLOR_DARK_GREY, false);
-        drawRow(rowMid, SET_BG_COLOR_LIGHT_GREY, false);
-        drawRow(rowMid, SET_BG_COLOR_DARK_GREY, false);
-        drawRow(blackRow2, SET_BG_COLOR_LIGHT_GREY, false);
-        drawRow(blackRow1, SET_BG_COLOR_DARK_GREY, false);
+        drawEdgeRow(edgeRow, false);
+        drawMiddleRow(whiteRow1, SET_BG_COLOR_LIGHT_GREY, false);
+        drawMiddleRow(whiteRow2, SET_BG_COLOR_BLACK, false);
+        drawMiddleRow(rowMid1, SET_BG_COLOR_LIGHT_GREY, false);
+        drawMiddleRow(rowMid2, SET_BG_COLOR_BLACK, false);
+        drawMiddleRow(rowMid3, SET_BG_COLOR_LIGHT_GREY, false);
+        drawMiddleRow(rowMid4, SET_BG_COLOR_BLACK, false);
+        drawMiddleRow(blackRow2, SET_BG_COLOR_LIGHT_GREY, false);
+        drawMiddleRow(blackRow1, SET_BG_COLOR_BLACK, false);
+        drawEdgeRow(edgeRow, false);
 
         System.out.println("\n");
 
         // white down
-        drawRow(blackRow1, SET_BG_COLOR_LIGHT_GREY, true);
-        drawRow(blackRow2, SET_BG_COLOR_DARK_GREY, true);
-        drawRow(rowMid, SET_BG_COLOR_LIGHT_GREY, true);
-        drawRow(rowMid, SET_BG_COLOR_DARK_GREY, true);
-        drawRow(rowMid, SET_BG_COLOR_LIGHT_GREY, true);
-        drawRow(rowMid, SET_BG_COLOR_DARK_GREY, true);
-        drawRow(whiteRow2, SET_BG_COLOR_LIGHT_GREY, true);
-        drawRow(whiteRow1, SET_BG_COLOR_DARK_GREY, true);
+        drawEdgeRow(edgeRow, true);
+        drawMiddleRow(blackRow1, SET_BG_COLOR_LIGHT_GREY, true);
+        drawMiddleRow(blackRow2, SET_BG_COLOR_BLACK, true);
+        drawMiddleRow(rowMid1, SET_BG_COLOR_LIGHT_GREY, true);
+        drawMiddleRow(rowMid2, SET_BG_COLOR_BLACK, true);
+        drawMiddleRow(rowMid3, SET_BG_COLOR_LIGHT_GREY, true);
+        drawMiddleRow(rowMid4, SET_BG_COLOR_BLACK, true);
+        drawMiddleRow(whiteRow2, SET_BG_COLOR_LIGHT_GREY, true);
+        drawMiddleRow(whiteRow1, SET_BG_COLOR_BLACK, true);
+        drawEdgeRow(edgeRow, true);
     }
 }

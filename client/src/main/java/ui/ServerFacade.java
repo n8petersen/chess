9f031fd299d8c02f355d5e8e956419a996b839e5
n8petersen <n8knows@gmail.com>
2024-06-09 22:35:39 -0600

    add server requests

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -5,10 +5,10 @@ import com.google.gson.Gson;
 import model.AuthData;
 import model.GameData;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.*;
+
 import java.util.Map;
 
 public class ServerFacade {
@@ -21,71 +21,53 @@ public class ServerFacade {
     }
 
     public AuthData register(String username, String password, String email) throws Exception {
-        AuthData auth = null;
-        URL url = (new URI(hostname + "/user")).toURL();
-        var req = Map.of("username", username, "password", password, "email", email);
-        var reqJson = serializer.toJson(req);
-        HttpURLConnection http = (HttpURLConnection) url.openConnection();
-        http.setRequestMethod("POST");
-        http.setDoOutput(true);
-        http.addRequestProperty("Accept", "application/json");
-        var outStream = http.getOutputStream();
-        outStream.write(reqJson.getBytes());
-        http.connect();
-        try (InputStream respBody = http.getInputStream()) {
-            InputStreamReader inputStreamReader = new InputStreamReader(respBody);
-            auth = serializer.fromJson(inputStreamReader, AuthData.class);
-        }
-        return auth;
+        var body = Map.of("username", username, "password", password, "email", email);
+        return sendRequest("POST", "/user", body, null, AuthData.class);
     }
 
     public AuthData login(String username, String password) throws Exception {
-        AuthData auth = null;
-        URL url = (new URI(hostname + "/session")).toURL();
-        var req = Map.of("username", username, "password", password);
-        var reqJson = serializer.toJson(req);
-        HttpURLConnection http = (HttpURLConnection) url.openConnection();
-        http.setRequestMethod("POST");
-        http.setDoOutput(true);
-        http.addRequestProperty("Accept", "application/json");
-        var outStream = http.getOutputStream();
-        outStream.write(reqJson.getBytes());
-        http.connect();
-        try (InputStream respBody = http.getInputStream()) {
-            InputStreamReader inputStreamReader = new InputStreamReader(respBody);
-            auth = serializer.fromJson(inputStreamReader, AuthData.class);
-        }
-        return auth;
+        var body = Map.of("username", username, "password", password);
+        return sendRequest("POST", "/session", body, null, AuthData.class);
     }
 
     public void logout(String authToken) throws Exception {
-        URL url = (new URI(hostname + "/session")).toURL();
+        sendRequest("DELETE", "/session", null, authToken, null);
+    }
+
+    public GameData createGame(String authToken, String gameName) throws Exception {
+        var body = Map.of("gameName", gameName);
+        return sendRequest("POST", "/game", body, authToken, GameData.class);
+    }
+
+    public GameList listGames(String authToken) throws Exception {
+        return sendRequest("GET", "/game", null, authToken, GameList.class);
+    }
+
+    public GameData joinGame(String authToken, int gameID, ChessGame.TeamColor color) throws Exception {
+        var body = Map.of("playerColor", color, "gameID", gameID);
+        return sendRequest("PUT", "/game", body, authToken, GameData.class);
+    }
+
+    private <T> T sendRequest(String httpMethod, String endpoint, Object body, String authToken, Class<T> classType) throws Exception {
+        URL url = (new URI(hostname + endpoint)).toURL();
         HttpURLConnection http = (HttpURLConnection) url.openConnection();
-        http.setRequestMethod("DELETE");
-        http.addRequestProperty("authorization", authToken);
+        http.setRequestMethod(httpMethod);
+        http.setDoOutput(true);
+        if (authToken != null) {
+            http.addRequestProperty("authorization", authToken);
+        }
+        if (body != null) {
+            http.addRequestProperty("Accept", "application/json");
+            var bodyJson = serializer.toJson(body);
+            var outStream = http.getOutputStream();
+            outStream.write(bodyJson.getBytes());
+        }
         http.connect();
         InputStream respBody = http.getInputStream();
-    }
-
-    public GameData createGame(String authToken, String gameName) {
-
-
+        InputStreamReader inputStreamReader = new InputStreamReader(respBody);
+        if (classType != null) {
+            return serializer.fromJson(inputStreamReader, classType);
+        }
         return null;
-    }
-
-    public GameData[] listGames(String authToken) {
-
-
-        return null;
-    }
-
-    public GameData joinGame(String authToken, int gameId, ChessGame.TeamColor color) {
-
-
-        return null;
-    }
-
-    public void clear() {
-
     }
 }

1f46c1f818c6aa5c9c3d3ea02a2ec23ff10c6e67
n8petersen <n8knows@gmail.com>
2024-06-16 22:46:14 -0600

    change back to regular class

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,12 +1,26 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
-public record ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
+public final class ChessMove {
+    private final ChessPosition startPosition;
+    private final ChessPosition endPosition;
+    private final ChessPiece.PieceType promotionPiece;
+
+    /**
+     *
+     */
+    public ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
+    }
 
     /**
      * @return ChessPosition of starting location
@@ -21,5 +35,33 @@ public record ChessMove(ChessPosition startPosition, ChessPosition endPosition, 
     public ChessPosition getEndPosition() {
         return endPosition;
     }
+
+    public ChessPiece.PieceType getPromotionPiece() {
+        return promotionPiece;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ChessMove) obj;
+        return Objects.equals(this.startPosition, that.startPosition) &&
+                Objects.equals(this.endPosition, that.endPosition) &&
+                Objects.equals(this.promotionPiece, that.promotionPiece);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPosition, endPosition, promotionPiece);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessMove[" +
+                "startPosition=" + startPosition + ", " +
+                "endPosition=" + endPosition + ", " +
+                "promotionPiece=" + promotionPiece + ']';
+    }
+
 
 }

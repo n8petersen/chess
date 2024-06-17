54d3ae5b1ddfa842378622453792725fa298ba19
n8petersen <n8knows@gmail.com>
2024-06-16 20:17:44 -0600

    dynamically draw board

--- client/src/main/java/ui/Draw.java
+++ client/src/main/java/ui/Draw.java
@@ -6,12 +6,27 @@ import model.GameData;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 import static ui.EscapeSequences.*;
 
 public class Draw {
 
     private final PrintStream out;
+    private static final Map<String, String> pieceMap = Map.ofEntries(
+            Map.entry("WHITE_KING", WHITE_KING),
+            Map.entry("WHITE_QUEEN", WHITE_QUEEN),
+            Map.entry("WHITE_BISHOP", WHITE_BISHOP),
+            Map.entry("WHITE_KNIGHT", WHITE_KNIGHT),
+            Map.entry("WHITE_ROOK", WHITE_ROOK),
+            Map.entry("WHITE_PAWN", WHITE_PAWN),
+            Map.entry("BLACK_KING", BLACK_KING),
+            Map.entry("BLACK_QUEEN", BLACK_QUEEN),
+            Map.entry("BLACK_BISHOP", BLACK_BISHOP),
+            Map.entry("BLACK_KNIGHT", BLACK_KNIGHT),
+            Map.entry("BLACK_ROOK", BLACK_ROOK),
+            Map.entry("BLACK_PAWN", BLACK_PAWN)
+    );
 
     public Draw() {
         out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
@@ -32,7 +47,7 @@ public class Draw {
                 colNums = new int[] {0,1,2,3,4,5,6,7};
                 colLetters = "    a  b  c  d  e  f  g  h    ";
             } else {
-                rowNums = new int[] {1,2,3,4,5,6,7};
+                rowNums = new int[] {0,1,2,3,4,5,6,7};
                 colNums = new int[] {7,6,5,4,3,2,1,0};
                 colLetters = "    h  g  f  e  d  c  b  a    ";
             }
@@ -42,8 +57,23 @@ public class Draw {
                 stringBuilder.append(SET_BG_COLOR_DARK_GREY).append(printRow).append(RESET_BG_COLOR);
                 for (var col : colNums) {
                     var piece = board.getPiece(new ChessPosition(row+1, col+1));
+                    var squareColor = (currentSquareWhite) ? SET_BG_COLOR_WHITE : SET_BG_COLOR_BLACK;
+                    if (piece != null) {
+                        var pieceColor = piece.getTeamColor().toString().toUpperCase();
+                        var pieceType = piece.getPieceType().toString().toUpperCase();
+
+                        var piecePrintColor = (pieceColor.equals("WHITE")) ? SET_TEXT_COLOR_BLUE : SET_TEXT_COLOR_RED;
+                        var pieceText = pieceMap.get(pieceColor + "_" + pieceType);
+                        stringBuilder.append(squareColor).append(piecePrintColor).append(pieceText).append(RESET_BG_COLOR).append(RESET_TEXT_COLOR);
+                    } else {
+                        stringBuilder.append(squareColor).append(EMPTY).append(RESET_BG_COLOR);
+                    }
+                    currentSquareWhite = !currentSquareWhite;
                 }
+                stringBuilder.append(SET_BG_COLOR_DARK_GREY).append(printRow).append(RESET_BG_COLOR).append('\n');
+                currentSquareWhite = !currentSquareWhite;
             }
+            stringBuilder.append(SET_BG_COLOR_DARK_GREY).append(colLetters).append(RESET_BG_COLOR).append('\n');
         } catch (Exception ex) {
             out.print("Error drawing board");
         }

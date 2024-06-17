65220a2ef48bcbdb39f4d74b8169c24434f46c83
n8petersen <n8knows@gmail.com>
2024-06-17 11:28:18 -0600

    create connectionmanager class

---
+++ server/src/main/java/server/handlers/websocket/ConnectionManager.java
@@ -0,0 +1,41 @@
+package server.handlers.websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+    public void add(String username, Connection connection) {
+        connections.put(username, connection);
+    }
+
+    public void remove(Session session) {
+        for (var conn : connections.values()) {
+            if (conn.session.equals(session)) {
+                connections.remove(conn.userData.username());
+            }
+        }
+    }
+
+    public Connection get(String username) {
+        return connections.get(username);
+    }
+
+    public void broadcast(int gameID, String sendingUser, String message) throws Exception {
+        for (var conn : connections.values()) {
+            if (conn.gameData.gameID() == gameID && !sendingUser.equals(conn.userData.username())) {
+                conn.send(message);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ConnectionManager{" +
+                "connections=" + connections +
+                '}';
+    }
+}

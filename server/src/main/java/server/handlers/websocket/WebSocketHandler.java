13c999b394370011d1d531c21f897ded2652de58
n8petersen <n8knows@gmail.com>
2024-06-17 16:46:49 -0600

    add make move

--- server/src/main/java/server/handlers/websocket/WebSocketHandler.java
+++ server/src/main/java/server/handlers/websocket/WebSocketHandler.java
@@ -1,7 +1,9 @@
 package server.handlers.websocket;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
 import dataaccess.DataAccess;
+import model.*;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import websocket.commands.*;
@@ -29,9 +31,17 @@ public class WebSocketHandler {
     public void onMessage(Session session, String message) throws Exception {
         try {
             var command = new Gson().fromJson(message, UserGameCommand.class);
+
+            var authData = dataAccess.authDAO().readAuth(command.getAuthString());
+            var userData = dataAccess.userDAO().readUser(authData.username());
+            var gameData = dataAccess.gameDAO().readGame(command.getGameID());
+
+            var connection = connectionManager.add(userData.username(), new Connection(userData, session));
+            connection.gameData = gameData;
+
             switch (command.getCommandType()) {
-                case CONNECT -> connect(command, session);
-                case MAKE_MOVE -> move();
+                case CONNECT -> connect(connection, gameData, userData);
+                case MAKE_MOVE -> move(command, connection, gameData, userData);
                 case LEAVE -> leave();
                 case RESIGN -> resign();
             }
@@ -41,18 +51,11 @@ public class WebSocketHandler {
         }
     }
 
-    private void connect(UserGameCommand command, Session session) throws Exception {
-        var authData = dataAccess.authDAO().readAuth(command.getAuthString());
-        var userData = dataAccess.userDAO().readUser(authData.username());
-        var gameData = dataAccess.gameDAO().readGame(command.getGameID());
-        var connection = new Connection(userData, session);
-        connection = connectionManager.add(userData.username(), connection);
-
+    private void connect(Connection connection, GameData gameData, UserData userData) throws Exception {
         if (gameData != null) {
             connection.gameData = gameData;
             var loadMessage = new LoadMessage(gameData).toString();
             connection.send(loadMessage);
-            String notificationMsg;
 
             String teamColor;
             if (userData.username().equalsIgnoreCase(gameData.whiteUsername())) {
@@ -63,14 +66,38 @@ public class WebSocketHandler {
                 teamColor = "observer";
             }
 
-            notificationMsg = (new NotificationMessage(String.format("%s joined game '%s' as %s", connection.userData.username(), gameData.gameName(), teamColor))).toString();
-            connectionManager.broadcast(gameData.gameID(), connection.userData.username(), notificationMsg);
+            String notificationMessage = (new NotificationMessage(String.format("%s joined game '%s' as %s", connection.userData.username(), gameData.gameName(), teamColor))).toString();
+            connectionManager.broadcast(gameData.gameID(), connection.userData.username(), notificationMessage);
         } else {
-            connection.sendError("Couldn't find game");
+            connection.sendError("couldn't find game");
         }
     }
 
-    private void move() {
+    private void move(UserGameCommand command, Connection connection, GameData gameData, UserData userData) throws Exception {
+        if (gameData != null) {
+            var turnColor = gameData.game().getTeamTurn();
+            var pieceColor = gameData.game().getBoard().getPiece(command.getMove().getStartPosition()).getTeamColor();
+            ChessGame.TeamColor playerColor = null;
+            if (userData.username().equalsIgnoreCase(gameData.whiteUsername())) {
+                playerColor = ChessGame.TeamColor.WHITE;
+            } else if (userData.username().equalsIgnoreCase(gameData.blackUsername())) {
+                playerColor = ChessGame.TeamColor.BLACK;
+            }
+            if (playerColor != null && playerColor.equals(pieceColor) && playerColor.equals(turnColor)) {
+                String notificationMessage = (new NotificationMessage(String.format("%s made move%s", userData.username(), command.getMove()))).toString();
+                gameData.game().makeMove(command.getMove());
+                connectionManager.broadcast(gameData.gameID(), connection.userData.username(), notificationMessage);
+                dataAccess.gameDAO().updateGame(gameData);
+                connection.gameData = gameData;
+
+                String loadMessage = new LoadMessage(gameData).toString();
+                connectionManager.broadcast(gameData.gameID(), "", loadMessage);
+            } else {
+                connection.sendError("invalid move");
+            }
+        } else {
+            connection.sendError("couldn't find game");
+        }
     }
 
     private void leave() {

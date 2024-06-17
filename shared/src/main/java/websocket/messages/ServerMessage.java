79644222f40fc0ba140647a1d4c4de249123f29c
n8petersen <n8knows@gmail.com>
2024-06-17 15:59:16 -0600

    cleanup

--- shared/src/main/java/websocket/messages/ServerMessage.java
+++ shared/src/main/java/websocket/messages/ServerMessage.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 
 /**
  * Represents a Message the server can send through a WebSocket
- * 
  * Note: You can add to this class, but you should not alter the existing
  * methods.
  */

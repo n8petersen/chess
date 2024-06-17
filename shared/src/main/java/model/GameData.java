d449d4d2f0866005b43fbad4885fea1c8ab38371
n8petersen <n8knows@gmail.com>
2024-06-17 16:58:00 -0600

    add color setters

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -4,4 +4,11 @@ import chess.ChessGame;
 
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
 
+    public GameData setWhite(String username) {
+        return new GameData(this.gameID, username, this.blackUsername, this.gameName, this.game);
+    }
+
+    public GameData setBlack(String username) {
+        return new GameData(this.gameID, this.whiteUsername, username, this.gameName, this.game);
+    }
 }

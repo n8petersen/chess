f605b84a623061b66978d44c1f923b75352c4b94
n8petersen <n8knows@gmail.com>
2024-06-17 13:42:17 -0600

    serialize server messages

--- shared/src/main/java/websocket/messages/ServerMessage.java
+++ shared/src/main/java/websocket/messages/ServerMessage.java
@@ -1,5 +1,7 @@
 package websocket.messages;
 
+import com.google.gson.Gson;
+
 import java.util.Objects;
 
 /**
@@ -38,5 +40,10 @@ public class ServerMessage {
     @Override
     public int hashCode() {
         return Objects.hash(getServerMessageType());
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
     }
 }

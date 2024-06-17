177da07f3bd12e55bc8c39b16bfa972c7e2060e1
n8petersen <n8knows@gmail.com>
2024-06-17 11:28:08 -0600

    create connection class

---
+++ server/src/main/java/server/handlers/websocket/Connection.java
@@ -0,0 +1,39 @@
+package server.handlers.websocket;
+
+import model.GameData;
+import model.UserData;
+import org.eclipse.jetty.websocket.api.RemoteEndpoint;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.*;
+
+public class Connection {
+    public UserData userData;
+    public GameData gameData;
+    public Session session;
+
+    public Connection(UserData user, Session session) {
+        this.userData = user;
+        this.session = session;
+    }
+
+    void send(String message) throws Exception {
+        session.getRemote().sendString(message);
+    }
+
+    void sendError(String message) throws Exception {
+        sendError(session.getRemote(), message);
+    }
+
+    static void sendError(RemoteEndpoint endpoint, String message) throws Exception {
+        var errorMessage = (new ErrorMessage(String.format("ERROR: %s", message))).toString();
+        endpoint.sendString(errorMessage);
+    }
+
+    @Override
+    public String toString() {
+        return "Connection{" +
+                "user=" + userData.username() +
+                ", game=" + gameData.gameID() +
+                '}';
+    }
+}
